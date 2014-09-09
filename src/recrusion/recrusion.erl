bc385545087305b71e7080a97987c670d02470a2
Wesley <wesley.olis@gmail.com>
2014-09-09 16:55:25 +0200

    Testing TCO and LCO

--- src/recrusion/recrusion.erl
+++ src/recrusion/recrusion.erl
@@ -1,5 +1,5 @@
 -module(recrusion).
--export([sublist/2]).
+-export([sublist/2, mergeAsTuple/2]).
 
 -ifdef(TEST).
 -compile([export_all]).
@@ -11,3 +11,9 @@ sublist_tail( _, 0, SubList ) -> SubList;
 sublist_tail( [], _, SubList ) -> SubList;
 sublist_tail( [H|T], N, SubList ) when N > 0 -> sublist_tail( T, N-1, [H | SubList] ).
 
+mergeAsTuple( LsA, LsB ) -> lists:reverse(mergeAsTuple_tail( LsA, LsB, [] )).
+
+mergeAsTuple_tail( [A|LsA], [B|LsB], Rs ) -> mergeAsTuple_tail( LsA, LsB, [ {A,B} | Rs ]);
+mergeAsTuple_tail( [], [B|LsB], Rs) -> mergeAsTuple_tail( [], LsB, [ {undefined, B} | Rs ] );
+mergeAsTuple_tail( [A|LsA], [], Rs) -> mergeAsTuple_tail( LsA, [], [ {A, undefined} | Rs ] );
+mergeAsTuple_tail( [], [], Rs ) -> Rs.
--- test/recrusion_tests.erl
+++ test/recrusion_tests.erl
@@ -3,6 +3,10 @@
 
 setup_test() -> ?assertEqual( b, b ).
 
-sublist_test() -> ?assertEqual( [4,3,2,1], recrusion:sublist([1,2,3,4,5,6,7,8],4) ).
+sublist_test() -> ?assertEqual( [4,3,2,1], recrusion:sublist([1,2,3,4,5,6,7,8],4) ),
+                  ?assertEqual([{1,5},{2,6},{3,7},{4,8}] , recrusion:mergeAsTuple([1,2,3,4],[5,6,7,8])),
+                  ?assertEqual([{1,5},{2,6},{3,7},{undefined,8}] , recrusion:mergeAsTuple([1,2,3],[5,6,7,8])),
+                  ?assertEqual([{1,5},{2,6},{3,undefined},{4,undefined}] , recrusion:mergeAsTuple([1,2,3,4],[5,6])),
+                  ?assertEqual([{1,undefined},{2,undefined},{3,undefined},{4,undefined}] , recrusion:mergeAsTuple([1,2,3,4],[])).
 
 teardown_test() -> ?assertEqual( a, a ).
